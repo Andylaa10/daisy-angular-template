1d505d70915bb6ae069c59c43f0c9589d6e05a20
Andy lam <andylaa100@gmail.com>
2026-04-01 21:45:32 +0200

    chore: added commitlint to husky

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -6,16 +6,50 @@ export default {
     questions: {
       type: {
         enum: {
+          feat: {
+            description: 'A new feature',
+            emoji: '✨ ',
+          },
+          fix: {
+            description: 'A bug fix',
+            emoji: '🐛 ',
+          },
+          docs: {
+            description: 'Documentation only changes',
+            emoji: '📚 ',
+          },
+          style: {
+            description:
+              'Code style changes (formatting, missing semi colons, etc)',
+            emoji: '💎 ',
+          },
+          refactor: {
+            description:
+              'Code change that neither fixes a bug nor adds a feature',
+            emoji: '📦 ',
+          },
+          perf: {
+            description: 'A code change that improves performance',
+            emoji: '🚀 ',
+          },
+          test: {
+            description: 'Adding missing tests or correcting existing tests',
+            emoji: '🧪 ',
+          },
           build: {
+            description: 'Changes that affect the build system or dependencies',
             emoji: '🛠️ ',
           },
-          chore: {
-            emoji: '♻️ ',
-          },
           ci: {
+            description: 'Changes to CI configuration files and scripts',
             emoji: '⚙️ ',
           },
+          chore: {
+            description: 'Other changes that don’t modify src or test files',
+            emoji: '♻️ ',
+          },
           revert: {
+            description: 'Reverts a previous commit',
             emoji: '🗑️ ',
           },
         },
